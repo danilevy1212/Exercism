cfa6872029a7a5eef75b464baeded0ad0ad0f550
danilevy1212 <danilevy1212@gmail.com>
2019-04-26 16:59:37 +0200

    Solved pangram.py

---
+++ python/pangram/pangram.py
@@ -0,0 +1,13 @@
+from string import ascii_lowercase
+from collections import OrderedDict
+import re
+
+def is_pangram(sentence):
+    let = {}
+    f_sen = filter(lambda c: c.isalpha(), sentence.lower())
+    
+    for c in f_sen:
+        if c not in let:
+            let[c] = 1
+
+    return len(let) == len(ascii_lowercase)
