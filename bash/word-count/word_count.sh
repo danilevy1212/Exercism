8ee981d31cdfd7cb1020563bba0521d9897ecba5
danilevy1212 <danilevy1212@gmail.com>
2019-05-08 00:12:00 +0200

    Revised and improved word_count.sh

--- bash/word-count/word_count.sh
+++ bash/word-count/word_count.sh
@@ -5,21 +5,16 @@ set -o errexit #Exit if there is an error
 set -o nounset #Exit if unset variables
 #set -x #Useful for debugging
 
-main(){
-    
-    filter=$(printf "$@" 2> /dev/null |tr '\n' ' '|tr ',' ' ')
-    input=(${filter,,})
+main(){    
+    local -A words
 
-    declare -A words
+    IFS=', ' read -ra input <<< "${1,,}"
 
     for word in "${input[@]}"; do
         filt_word=$(sed -e "s|^[[:punct:]]*||; s|[[:punct:]]*$||;" <<< ${word})
-
         local -i sum=${words[$filt_word]:-0}+1
         words["$filt_word"]=$sum
     done
-
-    res=''
 
     for K in "${!words[@]}"; do 
         echo "$K: ${words[$K]} "
