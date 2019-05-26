180fc4a29a4b7d9d224c712f8cd35aa4230bf29b
danilevy1212 <danilevy1212@gmail.com>
2019-05-26 22:50:59 +0200

    Completed forth.sh

---
+++ bash/forth/forth.sh
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+# set -x
+
+processMacro(){
+    local -a arr=( $1 )
+
+    if [[ ${arr[0]} == ':' ]]; then
+		if [[ ${arr[$((${#arr[@]}-1))]} != ';' ]]; then
+			echo "macro not terminated with semicolon" >&2 
+			exit 1
+		elif (( ${#arr[@]} < 4 )); then
+			echo "empty macro definition" >&2
+			exit 1
+		elif [[ ${arr[1]} =~ [0-9] ]]; then
+			echo "illegal operation" >&2
+			exit 1
+		else
+			local old_ifs=$IFS
+			macro[${arr[1]}]="$(for (( j=2; j<${#arr[@]}-1; j++ )); do echo "${arr[$j]} "; done)"
+		fi
+    fi
+}
+
+expand(){
+    local -i ind=$1 && shift
+    local -a arr=( $@ )
+    
+    (( $ind >=  ${#arr[@]} )) && { echo ${arr[@]}; return 0; } # Base-case
+
+    if [[ ${macro[${arr[$ind]}]+_} ]]; then
+		if [[ ${arr[0]} == ':' ]] && (( $ind == 1 )); then
+			ind+=1
+			expand $ind "${arr[@]}"
+		else
+			arr[$ind]=${macro[${arr[$ind]}]}
+			expand $ind "${arr[@]}"
+		fi
+	elif [[ ${arr[$ind]} == ':' ]] && (( $ind != 0 )); then
+		echo "wrong syntax"
+		return 1
+    elif [[ ${arr[$ind]} == ';' ]] && (( $ind <  $((${#arr[@]}-1)) )); then
+		echo "wrong syntax"
+		return 1
+    else
+		ind+=1
+		expand $ind "${arr[@]}"
+    fi
+}
+
+pop(){
+    poped=${stack[$((${#stack[@]}-1))]}
+    unset -v "stack[$((${#stack[@]}-1))]"
+}
+
+push(){
+    stack+=( $1 )
+}
+
+over(){
+    pop && local -i second=$poped && pop && first=$poped
+    push $first && push $second && push $first
+}
+
+swap(){
+    pop && local -i second=$poped && pop && first=$poped
+    push $second && push $first
+}
+
+dup(){
+    pop && local -i repl=$poped
+    push $repl && push $repl
+}
+
+div(){
+    pop && local -i second=$poped && pop && local -i first=$poped
+    (( $second > 0 )) && push $(($first/$second)) || { echo "Cannot divide by zero" >&2; exit 1; }
+}
+
+mult(){
+    pop && local -i second=$poped && pop && local -i first=$poped
+    push $(($first*$second))
+}
+
+sub(){
+    pop && local -i second=$poped && pop && local -i first=$poped
+    push $(($first-$second))
+}
+
+add(){
+    pop && local -i second=$poped && pop && local -i first=$poped
+    push $(($first+$second))
+}
+
+main(){  
+    declare -A inst=( ['+']="add" ['-']="sub" ['#']="mult" ['/']='div'
+		      ['dup']="dup" ['drop']="pop" ['swap']="swap" ['over']="over" )
+    declare -A macro=()
+    declare -a stack=()
+    declare -i poped
+
+    while read line; do
+		local tmp && tmp=`expand 0 "$(tr '*' '#' <<< "${line,,}")"` || { echo "$tmp" >&2; exit 1; }
+
+		[[ ${tmp:0:1} == ':' ]] && { processMacro "$tmp"; continue; }
+
+		declare -a input=( $tmp )
+		
+			for item in ${input[@]}; do
+				[[ "$item" =~ [0-9] ]] && { push "$item"; continue; }
+
+				[[ ${inst["$item"]+_} ]] || { echo "undefined operation: $item" >&2; exit 1; }
+				
+				case ${inst[$item]} in
+					'add'|'sub'|'mult'|'div'|'swap'|'over')
+						(( "${#stack[@]}" == 0 )) && { echo "empty stack" >&2; exit 1; }  
+						(( "${#stack[@]}" < 2 )) && { echo "only one value on the stack" >&2; exit 1; }
+						"${inst[$item]}"
+						;;
+					'dup'|'pop')
+						(( "${#stack[@]}" == 0 )) && { echo "empty stack" >&2; exit 1; }  
+						"${inst[$item]}"
+						;;
+					*)
+						echo "undefined operation: $item" >&2
+						exit 1
+						;;
+				esac
+		done
+    done < "${1:-/dev/stdin}"
+
+    echo "${stack[@]}"
+}
+
+main
---
+++ bash/forth/forth_test.sh
@@ -0,0 +1,412 @@
+#!/usr/bin/env bash
+
+# parsing and numbers
+@test numbers_only {
+    run bash forth.sh <<END
+1 2 3 4 5
+END
+    [[ $Status -eq 0 ]]
+    [[ $output == "1 2 3 4 5" ]]
+}
+
+# addition
+@test addition_ok {
+    run bash forth.sh <<END
+1 2 +
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "3" ]]
+}
+
+@test addition_no_args {
+    run bash forth.sh <<END
++
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"empty stack"* ]]
+}
+
+@test addition_one_args {
+    run bash forth.sh <<END
+1 +
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"only one value on the stack"* ]]
+}
+
+# subtraction
+@test subtraction_ok {
+    run bash forth.sh <<END
+3 4 -
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "-1" ]]
+}
+
+@test subtraction_no_args {
+    run bash forth.sh <<END
+-
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"empty stack"* ]]
+}
+
+@test subtraction_one_args {
+    run bash forth.sh <<END
+1 -
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"only one value on the stack"* ]]
+}
+
+# multiplication
+@test multiplication_ok {
+    run bash forth.sh <<END
+2 4 *
+END
+    echo $output
+    [[ $status -eq 0 ]]
+    [[ $output == "8" ]]
+}
+
+@test multiplication_no_args {
+    run bash forth.sh <<END
+*
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"empty stack"* ]]
+}
+
+@test multiplication_one_args {
+    run bash forth.sh <<END
+1 *
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"only one value on the stack"* ]]
+}
+
+# division
+@test division_ok {
+    run bash forth.sh <<END
+12 4 /
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "3" ]]
+}
+
+@test division_int_result {
+    run bash forth.sh <<END
+15 4 /
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "3" ]]
+}
+
+@test division_no_args {
+    run bash forth.sh <<END
+/
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"empty stack"* ]]
+}
+
+@test division_one_args {
+    run bash forth.sh <<END
+1 /
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"only one value on the stack"* ]]
+}
+
+@test division_by_zero {
+    run bash forth.sh <<END
+2 0 /
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"divide by zero"* ]]
+}
+
+# combined arithmetic
+@test add_and_subtract {
+    run bash forth.sh <<END
+1 2 + 4 -
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "-1" ]]
+}
+@test multiply_and_divide {
+    run bash forth.sh <<END
+2 4 * 3 /
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "2" ]]
+}
+
+# dup
+@test dup_1 {
+    run bash forth.sh <<END
+42 dup
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "42 42" ]]
+}
+
+@test dup_2 {
+    run bash forth.sh <<END
+1 2 dup
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1 2 2" ]]
+}
+
+@test dup_empty {
+    run bash forth.sh <<END
+dup
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"empty stack"* ]]
+}
+
+# drop
+@test drop_1 {
+    run bash forth.sh <<END
+42 drop
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "" ]]
+}
+
+@test drop_2 {
+    run bash forth.sh <<END
+1 2 drop
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1" ]]
+}
+
+@test drop_empty {
+    run bash forth.sh <<END
+drop
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"empty stack"* ]]
+}
+
+# swap
+@test swap_1 {
+    run bash forth.sh <<END
+1 2 swap
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "2 1" ]]
+}
+
+@test swap_2 {
+    run bash forth.sh <<END
+1 2 3 swap
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1 3 2" ]]
+}
+
+@test swap_empty {
+    run bash forth.sh <<END
+swap
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"empty stack"* ]]
+}
+
+@test swap_1arg {
+    run bash forth.sh <<END
+1 swap
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"only one value on the stack"* ]]
+}
+
+# over
+@test over_1 {
+    run bash forth.sh <<END
+1 2 over
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1 2 1" ]]
+}
+
+@test over_2 {
+    run bash forth.sh <<END
+1 2 3 over
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1 2 3 2" ]]
+}
+
+@test over_empty {
+    run bash forth.sh <<END
+over
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"empty stack"* ]]
+}
+
+@test over_1arg {
+    run bash forth.sh <<END
+1 over
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"only one value on the stack"* ]]
+}
+
+# user-defined words
+@test macro_with_builtin {
+    run bash forth.sh <<END
+: dup-twice dup dup ;
+1 dup-twice
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1 1 1" ]]
+}
+
+@test macro_maintain_order {
+    run bash forth.sh <<END
+: countup 1 2 3 ;
+countup
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1 2 3" ]]
+}
+
+@test macro_can_override_macro {
+    run bash forth.sh <<END
+: foo dup ;
+: foo swap ;
+1 2 foo
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "2 1" ]]
+}
+
+@test macro_can_override_builtin {
+    run bash forth.sh <<END
+: swap dup ;
+1 swap
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1 1" ]]
+}
+
+@test macro_can_override_operator {
+    run bash forth.sh <<END
+: + * ;
+3 4 +
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "12" ]]
+}
+
+@test macro_expand_in_macro_definition {
+    run bash forth.sh <<END
+: foo 5 ;
+: bar foo ;
+: foo 6 ;
+bar foo
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "5 6" ]]
+}
+
+@test macro_empty_definition {
+    run bash forth.sh <<END
+: foo ;
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"empty macro definition"* ]]
+}
+
+@test macro_expand_in_macro_redefinition {
+    run bash forth.sh <<END
+: foo 10 ;
+: foo foo 1 + ;
+foo
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "11" ]]
+}
+
+@test macro_cannot_redefine_numbers {
+    run bash forth.sh <<END
+: 1 2 ;
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"illegal operation"* ]]
+}
+
+@test macro_undefined {
+    run bash forth.sh <<END
+foo
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"undefined operation"* ]]
+}
+
+@test macro_missing_semicolon {
+    run bash forth.sh <<END
+: foo 1
+foo
+END
+    [[ $status -ne 0 ]]
+    [[ $output == *"macro not terminated with semicolon"* ]]
+}
+
+
+# case insensitivity
+@test case_dup {
+    run bash forth.sh <<END
+1 DUP Dup dup
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1 1 1 1" ]]
+}
+
+@test case_drop {
+    run bash forth.sh <<END
+1 2 3 4 DROP DrOp drop
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1" ]]
+}
+
+@test case_swap {
+    run bash forth.sh <<END
+1 2 SWAP 3 Swap 4 swap
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "2 3 4 1" ]]
+}
+
+@test case_over {
+    run bash forth.sh <<END
+1 2 OVER Over over
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1 2 1 2 1" ]]
+}
+
+@test case_macro_names {
+    run bash forth.sh <<END
+: foo dup ;
+1 FOO Foo foo
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1 1 1 1" ]]
+}
+
+@test case_macro_definitions {
+    run bash forth.sh <<END
+: SWAP DUP Dup dup ;
+1 swap
+END
+    [[ $status -eq 0 ]]
+    [[ $output == "1 1 1 1" ]]
+}
