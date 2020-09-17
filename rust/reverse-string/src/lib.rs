86cd9908b30a5ccba78347de2c8547b3e46e11ff
dlevym <danilevy1212>
2020-09-17 09:51:47 +0200

    Simplified the 'reverse-string' rust track solution.

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,16 +1,5 @@
-#[cfg(feature = "grapheme")]
-extern crate unicode_reverse;
+use unicode_segmentation::UnicodeSegmentation;
 
-#[cfg(feature = "grapheme")]
 pub fn reverse(input: &str) -> String {
-    let mut res = String::from(input);
-
-    unicode_reverse::reverse_grapheme_clusters_in_place(&mut res);
-
-    res
-}
-
-#[cfg(not(feature = "grapheme"))]
-pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+    input.graphemes(true).rev().collect()
 }
